d706a70f4d4edb54e59de19420e48d95e3a6f0a6
solsta <s.egorov@surrey.ac.uk>
2025-04-29 18:26:30 +0100

    Implemented lock free with benchmark

--- examples/queue_benchmarks/bench.cpp
+++ examples/queue_benchmarks/bench.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <atomic>
 #include <iostream>
 #include <string>
 #include <memory>
@@ -15,11 +16,20 @@ thread_local serialized_app_command serializedAppCommand;
 #define SM_OP_ENQUEUE 0
 #define SM_OP_DEQUEUE 1
 
+constexpr uint64_t POINTER_MASK = 0x0000FFFFFFFFFFFF;  // Lower 48 bits
+constexpr uint64_t COUNTER_MASK = 0xFFFF000000000000;  // Upper 16 bits
+
 // ---------------------- Node ----------------------
 template <size_t PayloadSize>
 struct Node {
     char payload[PayloadSize];
     Node* next = nullptr;
+};
+
+template <size_t PayloadSize>
+struct LockFreeNode {
+    char payload[PayloadSize];
+    LockFreeNode* next = nullptr;
 };
 
 // ---------------------- IQueue (templated interface) ----------------------
@@ -79,6 +89,10 @@ public:
     NodeType* front;
     NodeType* rear;
     
+    QueueB () {
+        front = rear = NULL;
+    }
+
     void enqueue(const char* payload) override {
         NodeType* newNode = (NodeType*)malloc(sizeof(NodeType));
         memcpy(newNode->payload, (void*)payload, sizeof(NodeType));
@@ -109,13 +123,101 @@ public:
 // ---------------------- QueueC implementation ----------------------
 template <typename NodeType>
 class QueueC : public IQueue<NodeType> {
+private:
+uint64_t pack(NodeType* ptr, uint16_t counter) {
+    uint64_t ptrBits = reinterpret_cast<uint64_t>(ptr) & POINTER_MASK;
+    uint64_t countBits = static_cast<uint64_t>(counter) << 48;
+    return ptrBits | countBits;
+
+}
+
+NodeType* unpack_ptr(uint64_t packed) {
+    return reinterpret_cast<NodeType*>(packed & POINTER_MASK);
+
+}
+
+uint16_t unpack_counter(uint64_t packed) {
+    return static_cast<uint16_t>(packed >> 48);
+
+}
+
 public:
+
+std::atomic<uint64_t> front; // This is actually a packed Pointer
+std::atomic<uint64_t> rear;
+
+QueueC(){
+    NodeType *node = (NodeType*)malloc(sizeof(NodeType));
+    uint64_t nodePtr = pack(node,0);
+    front = rear = nodePtr;
+}
     void enqueue(const char* payload) override {
-        std::cout << "[QueueC] Enqueue: " << payload << "\n";
+        //printf("In enqueue, Node size is :%d\n",sizeof(NodeType));
+
+        NodeType *node = (NodeType*)malloc(sizeof(NodeType));
+        memcpy(node->payload, (void*)payload, sizeof(NodeType));
+        node->next = nullptr;
+
+        uint16_t cachedTailCounter;
+        uint64_t cachedTail;
+        NodeType *cachedTailPtr;
+
+        while(true){
+            cachedTail = rear; // Cache the pointer and counter
+            cachedTailPtr = unpack_ptr(cachedTail);
+            cachedTailCounter = unpack_counter(cachedTail);
+    
+            uint64_t packedNext =  reinterpret_cast<uint64_t>(cachedTailPtr->next);
+    
+            NodeType *nextNode = unpack_ptr(packedNext);
+            //printf("l 170\n");
+            if(cachedTail == rear){
+                
+                if(nextNode == nullptr){ //Was tail pointing to the last node?
+                    //printf("l 176\n");
+                    cachedTailPtr->next = reinterpret_cast<NodeType*>(pack(node, cachedTailCounter+1));
+                    break;
+                } else{
+                    //printf("l 180\n");
+                    rear = pack(nextNode, cachedTailCounter+1);
+                }
+            }
+        }
+        //printf("l 185\n");
+        rear = pack(node, cachedTailCounter+1);
+        
     }
 
     NodeType* dequeue() override {
-        std::cout << "[QueueC] Dequeue\n";
+        //printf("In dequeue\n");
+        NodeType *cachedHeadPointer;
+        while (true)
+        {
+            uint64_t cachedHead = front;
+            uint64_t cachedTail = rear;
+    
+            cachedHeadPointer = unpack_ptr(cachedHead);
+            NodeType *cachedTailPointer = unpack_ptr(cachedTail);
+    
+            uint64_t packedNext =  reinterpret_cast<uint64_t>(cachedHeadPointer->next);
+            NodeType *nextNode = unpack_ptr(packedNext);
+    
+            uint64_t cachedTailCounter = unpack_counter(cachedTail);
+            uint64_t cachedHeadCounter = unpack_counter(cachedHead);
+            //printf("l 200\n");
+            if(cachedHead == front){
+                if(cachedHeadPointer == cachedTailPointer){
+                    if(nextNode == nullptr){
+                        return nullptr;
+                    }
+                  rear = pack(nextNode,cachedTailCounter+1);
+                } else{
+                    front = pack(nextNode,cachedHeadCounter+1);
+                    break;
+                }
+            }
+        }
+        free(cachedHeadPointer);
         return nullptr;
     }
 };
@@ -156,7 +258,14 @@ std::unique_ptr<AbstractQueue> make_queue(const char type, size_t payload_size) 
         return std::make_unique<QueueWrapper<Node<128-8>>>(new QueueB<Node<128-8>>());
     if (type == 'B' && payload_size == 256)
         return std::make_unique<QueueWrapper<Node<256-8>>>(new QueueB<Node<256-8>>());
-    
+
+
+    if (type == 'C' && payload_size == 64)
+        return std::make_unique<QueueWrapper<LockFreeNode<64-8>>>(new QueueC<LockFreeNode<64-8>>());
+    if (type == 'C' && payload_size == 128)
+        return std::make_unique<QueueWrapper<LockFreeNode<128-8>>>(new QueueC<LockFreeNode<128-8>>());
+    if (type == 'C' && payload_size == 256)
+        return std::make_unique<QueueWrapper<LockFreeNode<256-8>>>(new QueueC<LockFreeNode<256-8>>());
     return nullptr;
 }
 
@@ -185,9 +294,11 @@ void processRequest(serialized_app_command *serializedAppCommand){
 
 void benchmark_queue(int numThreads, int opsPerThread, AbstractQueue* q, size_t payload_size) {
     const int totalOps = numThreads * opsPerThread;
+    printf("Number of threads %d\n", numThreads);
     auto startTime = std::chrono::high_resolution_clock::now();
 
     std::vector<std::thread> workers;
+    
     for (int i = 0; i < numThreads; i++) {
 
         workers.emplace_back([=]() {
@@ -219,7 +330,7 @@ void benchmark_queue(int numThreads, int opsPerThread, AbstractQueue* q, size_t 
     auto endTime = std::chrono::high_resolution_clock::now();
     double elapsedSec = std::chrono::duration<double>(endTime - startTime).count();
 
-    printf("Node size : %d\n", payload_size);
+    printf("Node size : %ld\n", payload_size);
     printf("Total operations: %d\n", totalOps);
     printf("Elapsed time: %.6f seconds\n", elapsedSec);
     printf("Throughput: %.2f ops/sec\n", totalOps / elapsedSec);
@@ -248,6 +359,8 @@ int main(int argc, char** argv) {
     }
 
     AbstractQueue* q = queue.get();
+
+    printf("Populating queue\n");
 
     char buf[payload_size];
     for(int i=0; i < 128; i++){
