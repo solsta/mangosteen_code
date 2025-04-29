f647a11158c9da5015c4d076463b494347ec3df5
solsta <s.egorov@surrey.ac.uk>
2025-04-29 16:10:47 +0100

    Added lock based queue

--- examples/queue_benchmarks/bench.cpp
+++ examples/queue_benchmarks/bench.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <memory>
 #include <pthread.h>
+#include <string.h>
 
 // ---------------------- Node ----------------------
 template <size_t PayloadSize>
@@ -31,12 +32,50 @@ public:
 template <typename NodeType>
 class QueueA : public IQueue<NodeType> {
 public:
+
+NodeType* front;
+NodeType* rear;
+
+QueueA () {
+    NodeType *node = (NodeType*)malloc(sizeof(NodeType));
+    node->next = NULL;
+    front = rear = node;
+}
     void enqueue(const char* payload) override {
-        std::cout << "[QueueA] Enqueue: " << payload << "\n";
+        printf("Called enqueue\n");
+        NodeType* newNode = (NodeType*)malloc(sizeof(NodeType));
+        printf("Copying %d bytes\n",sizeof(NodeType));
+        printf("Src: %s\n", payload);
+        memcpy(newNode->payload, (void*)payload, sizeof(NodeType));
+        printf("Node payload: %s\n", newNode->payload);
+        newNode->next = NULL;
+        rear->next = newNode;
+        rear = newNode;
     }
 
     NodeType* dequeue() override {
-        std::cout << "[QueueA] Dequeue\n";
+        NodeType* node = front;
+        NodeType *new_head = node->next;
+        if (new_head == NULL) {
+            return NULL;
+        }
+        printf("Node payload: %s\n", new_head->payload);
+        front = new_head;
+        free(node);
+        return nullptr;
+    }
+};
+
+// ---------------------- QueueB implementation ----------------------
+template <typename NodeType>
+class QueueB : public IQueue<NodeType> {
+public:
+    void enqueue(const char* payload) override {
+        std::cout << "[QueueB] Enqueue: " << payload << "\n";
+    }
+
+    NodeType* dequeue() override {
+        std::cout << "[QueueB] Dequeue\n";
         return nullptr;
     }
 };
@@ -70,6 +109,13 @@ std::unique_ptr<AbstractQueue> make_queue(const std::string& type, size_t payloa
         return std::make_unique<QueueWrapper<Node<128>>>(new QueueA<Node<128>>());
     if (type == "QueueA" && payload_size == 256)
         return std::make_unique<QueueWrapper<Node<248>>>(new QueueA<Node<248>>());
+    
+    if (type == "QueueB" && payload_size == 64)
+        return std::make_unique<QueueWrapper<Node<64>>>(new QueueB<Node<64>>());
+    if (type == "QueueB" && payload_size == 128)
+        return std::make_unique<QueueWrapper<Node<128>>>(new QueueB<Node<128>>());
+    if (type == "QueueB" && payload_size == 256)
+        return std::make_unique<QueueWrapper<Node<248>>>(new QueueB<Node<248>>());
     
     return nullptr;
 }
