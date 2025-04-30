220ccad2aba39fd50a43fb0427eda0a7d1982b23
solsta <s.egorov@surrey.ac.uk>
2025-04-30 10:43:58 +0100

    updated clean up script

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,7 @@
 sleep 1
 echo "Running cleanup script"
 
-killall -9 redis kv_state_machine local_state_machine local_state_machine_multi_thread state_machine_rpc queue ms_lock_queue ms_lock_free > /dev/null 2>&1
+killall -9 redis kv_state_machine local_state_machine local_state_machine_multi_thread state_machine_rpc queue ms_lock_queue ms_lock_free queue_bench > /dev/null 2>&1
 
 rm /home/vagrant/mangosteen_pmem_data/test_outputs/r* > /dev/null 2>&1 
 rm /home/vagrant/mangosteen_pmem_data/test_outputs/maps/* > /dev/null 2>&1
